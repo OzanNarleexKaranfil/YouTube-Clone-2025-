809b00a415d633b8ffaa530db06bfc7d57a61138
ozan karanfil <ozannkaranfill@gmail.com>
2025-02-10 17:11:49 +0300

    TRPC setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({text: "Ozan"})
+
 return (
-  <div>
-    Gelecekte videolar yükleyeceğim!
-  </div>
+  <HydrateClient>
+    <Suspense fallback={<p>Yükleniyor...</p>}>
+    <ErrorBoundary fallback={<p>Hata...</p>}>
+      <PageClient />
+    </ErrorBoundary>
+    </Suspense>
+  </HydrateClient>
 )
 }
